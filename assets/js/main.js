0a18fc62b8d69a8e6a6d928ddffa19ebd9545d7e
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-11-22 11:06:34 +0200

    in progress 60%

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,31 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-
+/*=======MENU SHOW ======*/
+// Validation if constant exist
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+/*=======MENU HIDDEN ======*/
+// Validation if constant exist
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When user clicks on each nav__link, code removwe the show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
